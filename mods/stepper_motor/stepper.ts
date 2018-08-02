d2b0385fe5e1eae733e411b2b0750a13edf3690f
Andy Schulz-Jussenhofen <a.schulz-jussenhofen@sprinteins.com>
2018-08-02 21:56:49 +0200

    add stepper motor

--- mods/stepper_motor/stepper.ts
+++ mods/stepper_motor/stepper.ts
@@ -1,7 +1,5 @@
 import {Gpio} from 'onoff'; //include onoff to interact with the GPIO
 
-const pir: Gpio = new Gpio(23, 'in', 'both'); //use GPIO 23 as input, and 'both' button presses, and releases should be handled
-
 // 6, 13, 19, 26
 const sm1: Gpio = new Gpio(6, 'out', 'both');
 const sm2: Gpio = new Gpio(13, 'out', 'both');
@@ -18,16 +16,24 @@ Seq1.push([0,0,1,0]);
 Seq1.push([0,0,0,1]);
 
 
-async function setState() {
-    for(let step of Seq1) {
-        sm1.writeSync(step[0]);
-        sm2.writeSync(step[1]);
-        sm3.writeSync(step[2]);
-        sm4.writeSync(step[3]);
+let counter = 0;
 
-        console.log(`Step ${step} set.`);
-        await sleep(1000);
+async function setState() {
+
+
+    while (counter < 100000 ) {
+        for(let step of Seq1) {
+            sm1.writeSync(step[0]);
+            sm2.writeSync(step[1]);
+            sm3.writeSync(step[2]);
+            sm4.writeSync(step[3]);
+
+            console.log(`Step ${step} set.`);
+            await sleep(10);
+        }
+        counter++;
     }
+
 
 }
 
